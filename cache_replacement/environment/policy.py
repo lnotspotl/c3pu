20c75f3606b17c0c3ff765984328f8934477ebb9
kuba <lnotspotl@gmail.com>
2024-10-11 09:47:34 -0400

    Format

--- cache_replacement/environment/policy.py
+++ cache_replacement/environment/policy.py
@@ -14,57 +14,57 @@
 # limitations under the License.
 
 """Simple cache replacement baselines."""
+
 import abc
 import collections
 
 
 class ReplacementPolicy(abc.ABC):
-  """A policy that takes cache states and produces eviction decisions."""
+    """A policy that takes cache states and produces eviction decisions."""
 
-  @abc.abstractmethod
-  def action(self, state):
-    """Given a cache state, returns an eviction decision or no eviction.
+    @abc.abstractmethod
+    def action(self, state):
+        """Given a cache state, returns an eviction decision or no eviction.
 
-    Args:
-      state (State): returned by a CacheReplacementEnv.
+        Args:
+          state (State): returned by a CacheReplacementEnv.
 
-    Returns:
-      eviction_decision (int): index of the line to evict, or -1 for no
-        eviction.
-    """
+        Returns:
+          eviction_decision (int): index of the line to evict, or -1 for no
+            eviction.
+        """
 
 
 class LRU(ReplacementPolicy):
-  """Evicts the least-recently used line."""
+    """Evicts the least-recently used line."""
 
-  def __init__(self):
-    self._time = 0
-    self._access_times = collections.defaultdict()
+    def __init__(self):
+        self._time = 0
+        self._access_times = collections.defaultdict()
 
-  def action(self, state):
-    address = state.access[0]
-    self._access_times[address] = self._time
-    self._time += 1
-    if not state.evict:
-      return -1
+    def action(self, state):
+        address = state.access[0]
+        self._access_times[address] = self._time
+        self._time += 1
+        if not state.evict:
+            return -1
 
-    return min((self._access_times[line], index)
-               for index, line in enumerate(state.cache_lines))[1]
+        return min((self._access_times[line], index) for index, line in enumerate(state.cache_lines))[1]
 
 
 class RandomPolicy(ReplacementPolicy):
-  """Evicts randomly when the cache is full (Belady_0)."""
+    """Evicts randomly when the cache is full (Belady_0)."""
 
-  def __init__(self, random_state):
-    """Constructs around a random state to query for decisions.
+    def __init__(self, random_state):
+        """Constructs around a random state to query for decisions.
 
-    Args:
-      random_state (np.RandomState): used to generate random decisions.
-    """
-    self._random_state = random_state
+        Args:
+          random_state (np.RandomState): used to generate random decisions.
+        """
+        self._random_state = random_state
 
-  def action(self, state):
-    if not state.evict:
-      return -1
+    def action(self, state):
+        if not state.evict:
+            return -1
 
-    return self._random_state.randint(len(state.cache_lines))
+        return self._random_state.randint(len(state.cache_lines))
--- cache_replacement/policy_learning/cache_model/loss.py
+++ cache_replacement/policy_learning/cache_model/loss.py
@@ -19,56 +19,57 @@ import torch
 
 
 def top_1_log_likelihood(probs):
-  """Log-likelihood of the top option.
+    """Log-likelihood of the top option.
 
-  Args:
-    probs (torch.FloatTensor): size (batch_size, num_options), where the options
-      are sorted from highest to lowest.
+    Args:
+      probs (torch.FloatTensor): size (batch_size, num_options), where the options
+        are sorted from highest to lowest.
 
-  Returns:
-    loss (torch.FloatTensor): tensor of shape (batch_size) [-log p(top option)]
-  """
-  # 1e-8 for numerical stability
-  return -torch.log(probs[:, 0] + 1e-8)
+    Returns:
+      loss (torch.FloatTensor): tensor of shape (batch_size) [-log p(top option)]
+    """
+    # 1e-8 for numerical stability
+    return -torch.log(probs[:, 0] + 1e-8)
 
 
-def approx_ndcg(scores, relevances, alpha=10., mask=None):
-  """Computes differentiable estimate of NDCG of scores as following.
+def approx_ndcg(scores, relevances, alpha=10.0, mask=None):
+    """Computes differentiable estimate of NDCG of scores as following.
 
-  Uses the approximation framework from Qin et. al., 2008
+    Uses the approximation framework from Qin et. al., 2008
 
-    IDCG = sum_i (exp(rel[i]) - 1) / ln(i + 1)
-    DCG = sum_i (exp(rel[i]) - 1) / ln(pos(score, i) + 1)
-    pos(score, i) =
-      1 + sum_{j != i} exp(-alpha s_{i, j}) / (1 + exp(-alpha s_{i, j}))
-      (differentiable approximate position function)
-    s_{i, j} = scores[i] - scores[j]
-    NDCG loss = -DCG / IDCG
+      IDCG = sum_i (exp(rel[i]) - 1) / ln(i + 1)
+      DCG = sum_i (exp(rel[i]) - 1) / ln(pos(score, i) + 1)
+      pos(score, i) =
+        1 + sum_{j != i} exp(-alpha s_{i, j}) / (1 + exp(-alpha s_{i, j}))
+        (differentiable approximate position function)
+      s_{i, j} = scores[i] - scores[j]
+      NDCG loss = -DCG / IDCG
 
-  Args:
-    scores (torch.FloatTensor): tensor of shape (batch_size, num_elems).
-    relevances (torch.FloatTensor): tensor of same shape as scores (rel).
-    alpha (float): value to use in the approximate position function. The
-      approximation becomes exact as alpha tends toward inf.
-    mask (torch.ByteTensor | None): tensor of same shape as scores. Masks out
-      elements at index [i][j] if mask[i][j] = 0. Defaults to no masking.
+    Args:
+      scores (torch.FloatTensor): tensor of shape (batch_size, num_elems).
+      relevances (torch.FloatTensor): tensor of same shape as scores (rel).
+      alpha (float): value to use in the approximate position function. The
+        approximation becomes exact as alpha tends toward inf.
+      mask (torch.ByteTensor | None): tensor of same shape as scores. Masks out
+        elements at index [i][j] if mask[i][j] = 0. Defaults to no masking.
 
-  Returns:
-    ndcg (torch.FloatTensor): tensor of shape (batch_size).
-  """
-  def approx_positions(scores, alpha=10.):
-    # s_{i, j} (batch_size, num_elems)
-    diff = (scores.unsqueeze(-1).expand(-1, -1, scores.shape[1]) -
-            scores.unsqueeze(1).expand(-1, scores.shape[1], -1))
-    # Add 0.5 instead of 1, because s_{1, i} = 0.5 is included
-    return 0.5 + torch.sigmoid(alpha * diff).sum(1)
+    Returns:
+      ndcg (torch.FloatTensor): tensor of shape (batch_size).
+    """
 
-  if mask is None:
-    mask = torch.ones_like(scores)
+    def approx_positions(scores, alpha=10.0):
+        # s_{i, j} (batch_size, num_elems)
+        diff = scores.unsqueeze(-1).expand(-1, -1, scores.shape[1]) - scores.unsqueeze(1).expand(
+            -1, scores.shape[1], -1
+        )
+        # Add 0.5 instead of 1, because s_{1, i} = 0.5 is included
+        return 0.5 + torch.sigmoid(alpha * diff).sum(1)
 
-  # +1 because indexing starts at 1 in IDCG
-  idcg = torch.expm1(relevances) * mask.float() / torch.log1p(
-      torch.arange(scores.shape[-1]).float() + 1)
-  pos = approx_positions(scores, alpha)
-  dcg = torch.expm1(relevances) * mask.float() / torch.log1p(pos)
-  return -dcg.sum(-1) / (idcg.sum(-1) + 1e-8)
+    if mask is None:
+        mask = torch.ones_like(scores)
+
+    # +1 because indexing starts at 1 in IDCG
+    idcg = torch.expm1(relevances) * mask.float() / torch.log1p(torch.arange(scores.shape[-1]).float() + 1)
+    pos = approx_positions(scores, alpha)
+    dcg = torch.expm1(relevances) * mask.float() / torch.log1p(pos)
+    return -dcg.sum(-1) / (idcg.sum(-1) + 1e-8)
