2f2ce4467c0e5eb19c70ff631b60fbed46844f2e
kuba <lnotspotl@gmail.com>
2024-11-03 21:35:12 -0500

    Add missing parameters

--- task06/eval_mpki.py
+++ task06/eval_mpki.py
@@ -192,6 +192,8 @@ if __name__ == "__main__":
     parser.add_argument("--model_config_path", type=str)
     parser.add_argument("--checkpoint_path", type=str)
     parser.add_argument("--results_file", type=str)
+    parser.add_argument("--num_cpus", type=int, default=1)
+    parser.add_argument("--use_gpu", type=bool, default=False)
 
     args = parser.parse_args()
 
